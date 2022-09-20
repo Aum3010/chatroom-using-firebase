8bdd4e0a98a9e1ab6acde5129a45b4eb7ea80763
Aum Pandya <73585014+Aum3010@users.noreply.github.com>
2022-09-20 22:50:27 +0530

    SEO

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// Here the use of web vitals kit was done to get understanding of CLS = cumulative layout shift , FID = first input delay and FCP = first contentful paint
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
